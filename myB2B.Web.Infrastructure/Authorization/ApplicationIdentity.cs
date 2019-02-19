218da98292f3a46b3839f31914b078b22cb32f0c
Rafał Palej <mail.rafixwpt@gmail.com>
2019-02-19 20:42:07 +0100

    #25: add objects to context
    #26: setup base identity objects

---
+++ myB2B.Web.Infrastructure/Authorization/ApplicationIdentity.cs
@@ -0,0 +1,13 @@
+﻿using myB2B.Domain;
+
+namespace myB2B.Web.Infrastructure.Authorization
+{
+    public class ApplicationIdentity
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string JwtToken { get; set; }
+
+        public int CompanyId { get; set; }
+    }
+}

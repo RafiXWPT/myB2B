87d5d31039e048f22c5b71fc0c607e1f430ccf82
Rafał Palej <mail.rafixwpt@gmail.com>
2019-02-22 23:50:43 +0100

    Deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,6 @@
 $configFiles = Get-ChildItem . appsettings.json -rec
 foreach($file in $configFiles) {
   (Get-Content $file.PSPath) |
-  Foreach-Object {$_ -replace "'ConnectionString': 'Server=localhost\\SQLEXPRESS;Database=MyB2B;Integrated Security=True;MultipleActiveResultSets=True;'", "$(sqlconnection)"} |
+  Foreach-Object {$_ -replace "Server=localhost\\SQLEXPRESS;Database=MyB2B;Integrated Security=True;MultipleActiveResultSets=True;", "$sqlconnection"} |
   Set-Content $file.PSPath
 }

ef331210392234eb6eb62b529eaf53dc388856cc
Rafał Palej <mail.rafixwpt@gmail.com>
2019-02-23 01:19:13 +0100

    localdb if Debug mode

--- deploy.ps1
+++ deploy.ps1
@@ -1,1 +1,2 @@
- ls . appsettings.json -rec | %{ $f=$_; (gc $f.PSPath) | %{ $_ -replace "CONNECTION_STRING", $(sqlconnection) } | sc $f.PSPath }
+param($sqlconnection="")
+ ls . appsettings.json -rec | %{ $f=$_; (gc $f.PSPath) | %{ $_ -replace "CONNECTION_STRING", $sqlconnection } | sc $f.PSPath }

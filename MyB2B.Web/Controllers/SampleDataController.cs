c29b1d41e769bdb7039792ccffd53210742db528
Rafał Palej <rapl@ailleron.com>
2019-07-26 11:22:04 +0200

    #125: hhhh

--- MyB2B.Web/Controllers/SampleDataController.cs
+++ MyB2B.Web/Controllers/SampleDataController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 
-namespace MyB2B.Web.Controllers
+namespace MyB2B.Web.Controllers 
 {
     [Route("api/[controller]")]
     public class SampleDataController : Controller

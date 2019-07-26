aa29d261b58a4c086e32c10a4d15e508630e5c8b
Rafał Palej <rapl@ailleron.com>
2019-07-26 11:41:51 +0200

    #1244:b

--- MyB2B.Web/Startup.cs
+++ MyB2B.Web/Startup.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting; 
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Controllers;

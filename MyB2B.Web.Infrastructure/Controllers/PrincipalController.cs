bc90ded948cfa679cfe750569d4aff14dbbb8860
Rafał Palej <mail.rafixwpt@gmail.com>
2019-08-03 21:12:55 +0200

    fix in getting proper httpcontext

--- MyB2B.Web.Infrastructure/Controllers/PrincipalController.cs
+++ MyB2B.Web.Infrastructure/Controllers/PrincipalController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Linq;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MyB2B.Web.Infrastructure.ApplicationUsers;
+using MyB2B.Web.Infrastructure.Dependency;
 
 namespace MyB2B.Web.Infrastructure.Controllers
 {
@@ -8,6 +10,6 @@ namespace MyB2B.Web.Infrastructure.Controllers
     {
         protected string CurrentUserEndpointAddress => ControllerContext?.HttpContext?.Request?.Host.Value;
         protected string RequestToken => HttpContext.GetAuthorizationTokenFromRequest();
-        public new ApplicationPrincipal User => HttpContext.ExtractApplicationPrincipal();
+        public new ApplicationPrincipal User => DependencyContainer.Container.GetInstance<IHttpContextAccessor>().HttpContext.ExtractApplicationPrincipal();
     }
 }

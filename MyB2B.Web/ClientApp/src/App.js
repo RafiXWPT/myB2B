de6f6830910e5197857483cb1dec307b520d1108
Rafał Palej <mail.rafixwpt@gmail.com>
2019-08-03 20:11:39 +0200

    Client side invoice logic

--- MyB2B.Web/ClientApp/src/App.js
+++ MyB2B.Web/ClientApp/src/App.js
@@ -8,6 +8,7 @@ import { TestToken } from './components/TestToken/TestToken';
 import { AccountAdministration } from './components/AccountAdministration/AccountAdministration';
 import {PrivateRoute} from './components/PrivateRoute';
 import { InvoiceGenerator } from './components/InvoiceGenerator/InvoiceGenerator';
+import { Invoices } from './components/Invoices/Invoices';
 
 export default class App extends Component {
   static displayName = App.name;
@@ -18,6 +19,7 @@ export default class App extends Component {
         <Route exact path='/' component={Home} />
         <Route path='/log-in' component={LogIn} />
         <Route path='/register' component={Register} />
+        <PrivateRoute path='/invoices' component={Invoices} />
         <PrivateRoute path='/account-administration' component={AccountAdministration} />
         <PrivateRoute path='/test-token' component={TestToken} />
         <PrivateRoute path='/test-invoice-generator' component={InvoiceGenerator} />

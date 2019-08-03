298d77a9ed17ff1e6b26f2f2ab213aafe9625bec
Rafał Palej <mail.rafixwpt@gmail.com>
2019-08-03 21:12:12 +0200

    invoice file dto refactor

---
+++ MyB2B.Web.Controllers.Logic/Invoice/Models/InvoiceFileDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MyB2B.Web.Controllers.Logic.Invoice.Models
+{
+    public class InvoiceFileDto
+    {
+        public string FileName { get; set; }
+        public string FileType { get; set; }
+        public byte[] FileContent { get; set; }
+    }
+}
